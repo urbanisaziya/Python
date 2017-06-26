274a0b461ce15ae1e980ff98c9b3939fd4c99fec
urbanisaziya <urbanisaziya@ya.ru>
2017-06-26 18:17:26 +0300

    commit test44

--- week2/44.py
+++ week2/44.py
@@ -2,6 +2,10 @@ a = int(input())
 b = int(input())
 c = int(input())
 d = int(input())
-(a*x+b)/(c*x+d) == 0
-
-print(x)
+D = (a*d + b*c)*2 - 4*a*c*b*d
+if D < 0:
+	print("NO")
+elif D == 0:
+	print("1")
+else:
+	print("2")
